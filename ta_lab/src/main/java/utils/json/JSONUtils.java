55c23b7eda5c9d2bd13159414a6af756874b91ca
Ulises Moralez <moralezulises@gmail.com>
2022-01-09 00:59:29 -0300

    Added JSON utils and Runner

---
+++ ta_lab/src/main/java/utils/json/JSONUtils.java
@@ -0,0 +1,29 @@
+package utils.json;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.logging.Logger;
+
+public class JSONUtils<T>{
+	private Logger LOGGER = Logger.getLogger( JSONUtils.class.getName());
+	ObjectMapper objectMapper = new ObjectMapper(  );
+	
+	public void ObjectToJSON( T t, String jsonPath ) throws IOException{
+		objectMapper.writeValue( new File( jsonPath ), t );
+	}
+	
+	public T JSONToObject(T t, String jsonPath) throws IOException{
+		T result = ( T ) objectMapper.readValue( new File( jsonPath ), t.getClass() );
+		return result;
+	}
+	
+	public ArrayList<T> JSONToObjectList(T t, String jsonPath) throws IOException{
+		ArrayList<T> result = objectMapper.readValue( new File( jsonPath ), new TypeReference< ArrayList< T > >( ){
+		} );
+		return  result;
+	}
+}
